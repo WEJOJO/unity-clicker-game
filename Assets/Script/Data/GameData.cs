4bff4c26d4879f6bc695abf60fb5daa1a4d95266
NAMIKTEST <skadlr650@gmail.com>
2019-01-24 17:53:07 +0900

    Script 바뀌기 전 / 2주차 복습 가능

---
+++ Assets/Script/Data/GameData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class GameData {
+
+	public int Gold;
+
+	public int GoldPerSec;
+
+	public int CollectGoldLevel;
+
+	public int Damage;
+
+	public int Defense;
+
+	public int Health;
+
+	public int DamageLevel;
+
+	public int DefenseLevel;
+
+	public int HealthLevel;
+
+	public int Level;
+
+	public int Exp;
+
+}
