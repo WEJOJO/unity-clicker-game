8589ff9b25a4c6b4e7801eeec0b07d9319c4c674
NAMIKTEST <skadlr650@gmail.com>
2019-01-24 14:42:26 +0900

    no message

---
+++ Assets/Script/GoldCounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldCounter : MonoBehaviour
+{
+    public int Gold=10;
+    public Text textGold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GoldUP());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IEnumerator GoldUP()
+        {
+            while(true)
+            {
+                yield return new WaitForSecondsRealtime(1f);
+                Gold++;
+                textGold.text=Gold.ToString();
+            }
+        }
+    }
+}
